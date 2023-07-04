117a1cc252b6161f3500d5496e03378d29af7812
julio yescas <yescasjulio7@gmail.com>
2023-07-04 09:54:16 -0600

    hover effect on navbar menu, and active option

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("conected");
+const myList = document.getElementById("nav-menu-list");
+const listItems = myList.getElementsByTagName("a");
+
+for (let i = 0; i < listItems.length; i++) {
+  const listItem = listItems[i];
+
+  // Agrega el evento de mouseover al elemento
+  listItem.addEventListener("mouseover", function () {
+    // Remueve la clase "myClass" de todos los elementos
+    for (let j = 0; j < listItems.length; j++) {
+      listItems[j].classList.remove("active");
+    }
+
+    // Agrega la clase "myClass" al elemento actual
+    listItem.classList.add("active");
+  });
+}
