1afec732a283c8e8b150c7ca58365ef5be8100aa
julio yescas <yescasjulio7@gmail.com>
2023-07-04 10:54:05 -0600

    changed som front aspect like glyphicons

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
+//** codigo para el menu inicial para computadora */
+
 const myList = document.getElementById("nav-menu-list");
 const listItems = myList.getElementsByTagName("a");
 
-for (let i = 0; i < listItems.length; i++) {
-  const listItem = listItems[i];
-
-  // Agrega el evento de mouseover al elemento
+Array.from(listItems).forEach((listItem) => {
   listItem.addEventListener("mouseover", function () {
-    // Remueve la clase "myClass" de todos los elementos
-    for (let j = 0; j < listItems.length; j++) {
-      listItems[j].classList.remove("active");
-    }
+    // Remueve la clase "active" de todos los elementos
+    Array.from(listItems).forEach((item) => {
+      item.classList.remove("active");
+    });
 
-    // Agrega la clase "myClass" al elemento actual
+    // Agrega la clase "active" al elemento actual
     listItem.classList.add("active");
   });
-}
+});
